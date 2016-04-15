2e75dbba6c4781a7b3ce23d1924acd67ffa0afd2
Jason Dong <JDong1@Stuy.edu>
2016-04-15 03:32:32 +0000

    activity 7 completed

--- pixLab/classes/PictureTester.java
+++ pixLab/classes/PictureTester.java
@@ -76,6 +76,22 @@ public class PictureTester
     temple.explore();
   }
 
+  public static void testMirrorArms()
+  {
+    Picture snowman = new Picture("snowman.jpg");
+    snowman.explore();
+    snowman.mirrorArms();
+    snowman.explore();
+  }
+  
+  public static void testMirrorGull()
+  {
+    Picture seagull = new Picture("seagull.jpg");
+    seagull.explore();
+    seagull.mirrorGull();
+    seagull.explore();
+  }
+
   /** Method to test the collage method */
   public static void testCollage()
   {
